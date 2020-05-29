4b06fbe718b0ce86a8ac616d850ac9517048591f
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-05-29 21:31:22 +0300

    Changes
    1) Add logic to button.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-let close = document.querySelector('.popup-close');
-let popup = document.querySelector('.popup');
-close.addEventListener('click', () => {
-popup.style.display = 'none';});
+// Кнопка 1
+
+let hideButton = createHideButtonEl();
+let hiddenBlock = createHiddenBlockEl();
+bindListenerToHideButton(hideButton, makeWindowHide);
+restoreHideButtonData();
+
+function createHideButtonEl() {
+  return document.querySelector(".js-btn-hide");
+}
+
+function createHiddenBlockEl() {
+  return document.querySelector(".hidden");
+}
+
+function bindListenerToHideButton(element, logic) {
+  element.addEventListener("click", logic);
+}
+
+function makeWindowHide() {
+  hiddenBlock.classList.toggle("hide");
+}
+
+function restoreHideButtonData() {}
+
+// Кнопка закрытия
+
+let close = document.querySelector(".popup-close");
+let popup = document.querySelector(".popup");
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
