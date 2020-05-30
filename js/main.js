af37a98f98731b7b31179e9a028169a0fca9b8c9
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-05-30 20:59:17 +0300

    Upd:
    1) Add modal window with JS.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
+"use strict";
+
+// Модальное окно
+
+createPopupWindow();
+
+function createPopupWindow() {
+  const popup = document.createElement("div");
+  popup.className = "popup";
+  popup.insertAdjacentHTML(
+    "beforeend",
+    `<div class="popup-container">
+        <div class="popup-content">
+           <button class="popup-close">&times;</button>
+           <h4 class="popup-header">Hello, World</h4>
+        </div>
+    </div>`
+  );
+
+  document.body.insertAdjacentElement("beforeend", popup);
+}
+
+// Кнопка закрытия модального окна
+
+const closeButton = getSelector("popup-close");
+const popup = getSelector("popup");
+
+function getSelector(selector) {
+  return document.querySelector(`.${selector}`);
+}
+
+closeButton.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
 // Кнопка 1
 
 const hideButton = getHideButtonEl();
 const hiddenBlock = getHiddenBlockEl();
 bindListenerToHideBtn(hideButton, makeWindowHide);
-restoreHideButtonData();
 
 function getHideButtonEl() {
   return document.querySelector(".js-btn-hide");
@@ -21,8 +55,6 @@ function makeWindowHide() {
   hiddenBlock.classList.toggle("collapse");
 }
 
-function restoreHideButtonData() {}
-
 // Кнопка 2
 
 let parentNode = getParentNode();
@@ -39,28 +71,14 @@ function bindListenerToSwapBtn(element, logic) {
   element.addEventListener("click", logic);
 }
 function swapNodeWrapper(parent) {
-
-   let clone = parent.children[1].cloneNode(true); // div1
-   let clone1 = parent.children[3].cloneNode(true); // div2
+  let clone = parent.children[1].cloneNode(true); // div1
+  let clone1 = parent.children[3].cloneNode(true); // div2
 
   return function swapNode() {
-   parent.removeChild(parent.children[1]);
-   parent.removeChild(parent.children[2]);
-   parent.insertBefore(clone1, parent.children[1]);
-   parent.insertBefore(clone, parent.children[3]);
-   [clone, clone1] = [clone1, clone];
-  }
+    parent.removeChild(parent.children[1]);
+    parent.removeChild(parent.children[2]);
+    parent.insertBefore(clone1, parent.children[1]);
+    parent.insertBefore(clone, parent.children[3]);
+    [clone, clone1] = [clone1, clone];
+  };
 }
-   
-// Модальное окно
-
-
-// Кнопка закрытия модального окна
-
-let closeButton = document.querySelector(".popup-close");
-let popup = document.querySelector(".popup");
-
-close.addEventListener("click", () => {
-  popup.style.display = "none";
-});
-
