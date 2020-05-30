9281b6bf12ef60ccf611bc520f961b816b999270
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-05-30 20:28:52 +0300

    Upd:
    1) Add Button 2 Logic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,66 @@
 // Кнопка 1
 
-let hideButton = createHideButtonEl();
-let hiddenBlock = createHiddenBlockEl();
-bindListenerToHideButton(hideButton, makeWindowHide);
+const hideButton = getHideButtonEl();
+const hiddenBlock = getHiddenBlockEl();
+bindListenerToHideBtn(hideButton, makeWindowHide);
 restoreHideButtonData();
 
-function createHideButtonEl() {
+function getHideButtonEl() {
   return document.querySelector(".js-btn-hide");
 }
 
-function createHiddenBlockEl() {
+function getHiddenBlockEl() {
   return document.querySelector(".hidden");
 }
 
-function bindListenerToHideButton(element, logic) {
+function bindListenerToHideBtn(element, logic) {
   element.addEventListener("click", logic);
 }
 
 function makeWindowHide() {
-  hiddenBlock.classList.toggle("hide");
+  hiddenBlock.classList.toggle("collapse");
 }
 
 function restoreHideButtonData() {}
 
-// Кнопка закрытия
+// Кнопка 2
 
-let close = document.querySelector(".popup-close");
+let parentNode = getParentNode();
+let swapButton = getSwapButton();
+bindListenerToSwapBtn(swapButton, swapNodeWrapper(parentNode));
+
+function getParentNode() {
+  return document.querySelector(".row");
+}
+function getSwapButton() {
+  return document.querySelector(".js-btn-swap");
+}
+function bindListenerToSwapBtn(element, logic) {
+  element.addEventListener("click", logic);
+}
+function swapNodeWrapper(parent) {
+
+   let clone = parent.children[1].cloneNode(true); // div1
+   let clone1 = parent.children[3].cloneNode(true); // div2
+
+  return function swapNode() {
+   parent.removeChild(parent.children[1]);
+   parent.removeChild(parent.children[2]);
+   parent.insertBefore(clone1, parent.children[1]);
+   parent.insertBefore(clone, parent.children[3]);
+   [clone, clone1] = [clone1, clone];
+  }
+}
+   
+// Модальное окно
+
+
+// Кнопка закрытия модального окна
+
+let closeButton = document.querySelector(".popup-close");
 let popup = document.querySelector(".popup");
+
 close.addEventListener("click", () => {
   popup.style.display = "none";
 });
+
