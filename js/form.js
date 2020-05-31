bb3202121a0bfa398cf74efa2749f3713bf978c6
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-05-31 14:32:05 +0300

    Upd:
    Some additions:
    - New Form.
    - New Files.

---
+++ js/form.js
@@ -0,0 +1,11 @@
+
+// Обработка формы
+
+let formButton = document.querySelector('.form-btn');
+buttonLogic(formButton);
+
+
+function buttonLogic (element) {
+element.addEventListener('click', () => {
+});
+}
